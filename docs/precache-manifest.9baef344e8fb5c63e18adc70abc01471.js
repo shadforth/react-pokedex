a1389ae09c115a3e37a0630f5c6d7e3fabca1730
Jacqui Shadforth <pwn@live.com.au>
2020-04-11 20:02:39 +1000

    Add production build

---
+++ docs/precache-manifest.9baef344e8fb5c63e18adc70abc01471.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "7bc78d4645edebd55b7fdf871efb980b",
+    "url": "/index.html"
+  },
+  {
+    "revision": "71504443468841f6075d",
+    "url": "/static/js/2.f21dbc1a.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/static/js/2.f21dbc1a.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "5599ebab9442c6b141c1",
+    "url": "/static/js/main.ee98de43.chunk.js"
+  },
+  {
+    "revision": "e3fadce71496b848919b",
+    "url": "/static/js/runtime-main.66b36cfa.js"
+  }
+]);
